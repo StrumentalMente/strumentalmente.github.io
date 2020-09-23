0c3a72b352c6700b00a3d642f159e9827caef975
espositoandrea <espositoandrea@users.noreply.github.com>
2020-09-23 17:52:02 +0000

    deploy: 7c77b4550add612a8b967243cef67c420b670a3e

---
+++ ftputils.rb
@@ -0,0 +1,34 @@
+require 'net/ftp'
+require 'colorize'
+
+def ftp_files(prefix_to_remove, source_files, target_dir, hostname, username, password, exclude_files)
+  Net::FTP.open(hostname, username, password) do |ftp|
+    begin
+      #   puts "Creating dir #{target_dir}"
+      ftp.mkdir target_dir
+    rescue StandardError
+      puts $ERROR_INFO
+    end
+    progressbar = ProgressBar.create(total: source_files.size, progress_mark: '#', format: '%t: |%B|'.yellow)
+    source_files.each do |source_file|
+      target_file = source_file.pathmap(prefix_to_remove ? "%{^#{prefix_to_remove},#{target_dir}}p" : "#{target_dir}%s%p")
+
+      next if exclude_files.include?(source_file)
+
+      begin
+        if File.directory?(source_file)
+          progressbar.log "Creating dir #{target_file}"
+          ftp.mkdir target_file
+        else
+          progressbar.log "Copying #{source_file} -> #{target_file}"
+          ftp.putbinaryfile(source_file, target_file)
+        end
+        progressbar.increment
+      rescue StandardError => e
+        progressbar.log "  => #{e.message.strip}".red
+      end
+    end
+    progressbar.finish
+    progressbar.stop
+  end
+end
